faa4b9b3ad5fe4674169d0ab935ab3e3a7ffe391
Paige Niedringhaus <paigen11@gmail.com>
2019-03-17 11:56:09 -0400

    adding the first jest test and function

---
+++ src/App.js
@@ -0,0 +1,6 @@
+export const add = (x, y) => {
+  return x + y;
+};
+
+// more concise, implicit return style function
+// export const add = (x, y) =>  x + y;
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
-test('Fake test', () => {
-  expect(true).toBeTruthy();
+// importing a named export (not a default), so the name must match and be in curly braces
+import { add } from './App';
+
+// unit test syntax
+// only tests one thing in the code (the add function)
+test('add function test', () => {
+  const value = add(1, 2);
+  expect(value).toBe(3);
 });
